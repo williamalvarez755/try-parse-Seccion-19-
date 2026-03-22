6b9aff004de6d5a4fcf8b16ad89483ba19b838b1
williamalvarez755 <williamalvarez1101@gmail.com>
2026-03-22 16:58:01 -0600

    subo otros 2 ejercicios

---
+++ ejercicio 3/ejercicio 3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ingrese cantidad de estudiantes del curos");
+int estudinates=0;
+while (!int.TryParse(Console.ReadLine(),out estudinates)||estudinates<0)
+{
+    Console.WriteLine("numero invalido");
+}
+Console.WriteLine("Numero de estudiantes es " + estudinates);
---
+++ ejercicio 4/ejercicio 4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("ingrese año de nacimiento ");
+int fecha;
+while (!int.TryParse(Console.ReadLine(),out fecha)||fecha<1950)
+{
+    Console.WriteLine("año invalido");
+}
+Console.WriteLine("año valido \n el año es " + fecha);
