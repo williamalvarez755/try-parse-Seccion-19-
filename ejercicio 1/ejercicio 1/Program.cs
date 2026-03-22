41cef97318f9518e1a80bf64e725e7631fb08d80
williamalvarez755 <williamalvarez1101@gmail.com>
2026-03-22 15:49:25 -0600

    subo primeros 2 ejercicios

---
+++ ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese edad");
+int edad;
+
+while (!int.TryParse(Console.ReadLine(), out edad) || edad < 0 || edad > 110)
+{
+    Console.WriteLine("Edad inválida, ingrese un valor entre 0 y 110");
+}
+
+Console.WriteLine($"Edad ingresada: {edad}");
---
+++ ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,7 @@
+﻿double precio = -1;
+Console.WriteLine("ingrese precio del producto ");
+while (!double.TryParse(Console.ReadLine(), out precio) || precio<0) 
+{
+    Console.WriteLine("precio invalido");
+}
+Console.WriteLine("precio es de " + precio );
